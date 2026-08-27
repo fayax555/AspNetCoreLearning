fc9ed109362d1a6d3af159ed39424362bc2f5915
fayax555 <fayax555@gmail.com>
2026-08-27 20:41:56 +0500

    Add category list page

---
+++ InventoryManager/Controllers/CategoriesController.cs
@@ -0,0 +1,21 @@
+﻿using InventoryManager.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryManager.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly InventoryDbContext _context;
+
+        public CategoriesController(InventoryDbContext dbContext)
+        {
+            _context = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var categories = _context.Categories.OrderBy(category => category.Name).ToList();
+            return View(categories);
+        }
+    }
+}
