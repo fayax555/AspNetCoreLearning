43e77b39af616e8ef7ee57e038fda8a6f9ef2133
fayax555 <fayax555@gmail.com>
2026-08-28 19:29:52 +0500

    Add product list page

---
+++ InventoryManager/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using InventoryManager.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryManager.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly InventoryDbContext _context;
+
+        public ProductsController(InventoryDbContext dbContext)
+        {
+            _context = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var products = _context.Products
+                .Include(product => product.Category)
+                .OrderBy(product => product.Name)
+                .ToList();
+            return View(products);
+        }
+    }
+}
