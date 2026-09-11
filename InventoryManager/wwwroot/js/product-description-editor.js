a969c3d4c123acc42806f9d6e9984ad7c68e125b
fayax555 <fayax555@gmail.com>
2026-09-11 19:37:35 +0500

    Extract reusable product description editor script

---
+++ InventoryManager/wwwroot/js/product-description-editor.js
@@ -0,0 +1,20 @@
+﻿const descriptionInput = document.querySelector('#Description');
+
+const quill = new Quill("#description-editor", {
+    theme: "snow",
+    modules: {
+        toolbar: [
+            ["bold", "italic", "underline"],
+            ["clean"]
+        ]
+    }
+});
+
+if (descriptionInput.value) {
+    quill.clipboard.dangerouslyPasteHTML(descriptionInput.value);
+}
+
+quill.on("text-change", () => {
+    const descriptionHasText = quill.getText().trim().length > 0;
+    descriptionInput.value = descriptionHasText ? quill.getSemanticHTML() : "";
+});
